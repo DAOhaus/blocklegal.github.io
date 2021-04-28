06605c7cb263cea50822fa290da95f0b8ce37e74
John D Storey <john@nft.org>
2021-04-28 14:00:14 -0400

    updated design

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-import logo from './logo.svg';
+import ReactJson from 'react-json-view'
 import './App.css';
+
+const info = {
+  name: 'Block Legal LLC',
+  description: 'combining new areas of technology with consistent and patient professionalism. We give each new client attention, information, and out of the box solutions. Our clients are our partners and are involved in the decision-making process while we find solutions.',
+  address: "270 Avenida Luis Muñoz Rivera San Juan, 00918 Puerto Rico",
+  email:'mmelendez@blolex.com',
+  phone: '787-632-9478'
+}
 
 function App() {
   return (
-    <div className="App" style={{padding: 20}}>
-      <div> block legal </div>
-      <br></br>
-      <div style={{width: 550, margin: 'auto'}}>
-      combining new areas of technology with consistent and patient professionalism. We give each new client attention, information, and out of the box solutions. Our clients are our partners and are involved in the decision-making process while we find solutions.
-      </div>
-      <br></br>
-      <div>
-      270 Avenida Luis Muñoz Rivera 
-      <br></br>
-      San Juan, 00918
-      <br></br>
-      Puerto Rico
-      </div>
-      <br></br>
-      <div> mmelendez@blolex.com </div>
-      <br></br>
-      <div> 787-632-9478 </div>
-      <br></br>
+    <div style={{padding: 20}}>
+      <ReactJson src={info} />
     </div>
   );
 }
