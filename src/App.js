61ab6d4cc887e1cb0968a4a8e546e3db8dcc97b4
j0xhn <john@daohaus.org>
2022-04-28 15:02:13 -0400

    updates with button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 const info = {
   name: 'Block Legal',
-  website: 'https://www.blocklegaloffice.com/',
+  webpage: 'https://www.blocklegaloffice.com',
   description: 'Combining new areas of technology with legal services. We give each new client attention, information, and out of the box solutions. Our clients are our partners and are involved in the decision-making process while we find solutions.',
   address: "270 Avenida Luis Muñoz Rivera, San Juan, 00918 Puerto Rico",
   phone: "787-632-9478"
@@ -11,8 +11,13 @@ const info = {
 
 function App() {
   return (
-    <div style={{padding: 20, fontSize: 16}}>
+    <div style={{ padding: 20, fontSize: 16 }}>
       <ReactJson src={info} />
+      <button style={{ margin: 20 }}>
+        <div >
+          <a target="_blank" href="https://www.blocklegaloffice.com/home-1">visit site</a>
+        </div>
+      </button>
     </div>
   );
 }
